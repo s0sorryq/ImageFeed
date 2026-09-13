8ae5fcf4c0e021c8dc9e9ea514e7644f0b8997c3
s0sorryq <clownclown2000@mail.ru>
2026-09-13 04:33:42 -0700

    Внесены изменения после ревью

--- ImageFeed/ImagesList/ImagesListCell.swift
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -4,7 +4,7 @@ final class ImagesListCell: UITableViewCell {
     static let reuseIdentifier = "ImagesListCell"
     
     
-    @IBOutlet var dateLabel: UILabel!
-    @IBOutlet var cellImage: UIImageView!
-    @IBOutlet var likeButton: UIButton!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var cellImage: UIImageView!
+    @IBOutlet weak var likeButton: UIButton!
 }
